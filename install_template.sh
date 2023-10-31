27927532468c25a23c9a30ec20379eddcde708ef
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-10-31 08:12:43 +0200

    Create install_template.sh

---
+++ install_template.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+PURPLE=$(tput setaf 5)
+NC=$(tput sgr0)
+
+echo "${PURPLE}Cloning Viper Xcode template${NC}"
+git clone https://github.com/TelemTobi/Viper-Module-Template.git /tmp/xcode_template
+
+echo "${PURPLE}Removing existing template folder${NC}"
+sudo rm -rf /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates/Modules/Viper.xctemplate
+
+echo "${PURPLE}Installing..${NC}"
+sudo mv /tmp/xcode_template/Viper.xctemplate "/Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File Templates/Modules/"
+
+echo "${PURPLE}Cleaning up${NC}"
+rm -rf /tmp/xcode_template
+
+echo "${PURPLE}Viper template installed successfully 🐍${NC}"
